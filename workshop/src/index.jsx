1f0361daaf48ed0c8c74d0485bfa2b3f48be8c5f
Brian Heise <bnheise@gmail.com>
2020-03-19 19:53:14 +0900

    added react component

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -2,3 +2,11 @@ import '../assets/stylesheets/application.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const root = document.querySelector("#root");
+
+const Hello = () => {
+  return <div> Hello there! </div>;
+};
+
+ReactDOM.render(<Hello />, root);
